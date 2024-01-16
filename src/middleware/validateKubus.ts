8cae8de52d09dffadf8353aa4b5d6c0d38b29ba7
z <112835955+dzuhazaky@users.noreply.github.com>
2024-01-16 09:27:59 +0700

    Add files via upload

---
+++ src/middleware/validateKubus.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express"
+import Joi from "joi"
+
+/** create validation schema */
+let schema = Joi.object({
+    panjang: Joi.number().required().min(1),
+    lebar: Joi.number().required().min(1),
+    tinggi: Joi.number().required().min(1),
+})
+
+/** create a validation function */
+let validateKubus = (request: Request, 
+    response: Response, 
+    next: NextFunction) => {
+    let { error } = schema.validate(request.body)
+    if (error) {
+        return response.status(400).json({
+            message: error.details
+        })
+    }
+    next()
+}
+
+export { validateKubus }
